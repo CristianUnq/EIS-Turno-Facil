fc09aa9bb990cae38120410b608c6b488590968f
CristianUnq <liotinecristian@hotmail.com>
2025-04-09 02:32:43 -0300

    commit inicial del proyecto Turno Facil

---
+++ src/main/java/com/proyecto/grupo6/turno_facil/TurnoFacilApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.grupo6.turno_facil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnoFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnoFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/grupo6/turno_facil/TurnoFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.grupo6.turno_facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnoFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
